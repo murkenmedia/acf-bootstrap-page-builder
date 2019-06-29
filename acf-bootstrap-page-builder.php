c2004c3a481fe23ecea5f5d7235d8c7e4e531dd0
murkenmedia <brian@murkenmedia.com>
2019-06-28 19:00:12 -0700

    Save custom classes as transient

--- acf-bootstrap-page-builder.php
+++ acf-bootstrap-page-builder.php
@@ -121,6 +121,10 @@ class Acf_Bootstrap_Page_Builder {
 		add_filter( 'plugin_row_meta', array( $this, 'plugin_row_links' ), 10, 2 );
 		
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueues' ) );
+		
+		add_action('acf/save_post', array( $this, 'clear_acfbs_transient' ), 20);
+		
+		//add_action( 'admin_menu', array( $this, 'acfbs_settings_page' ) );		
 
 		// add_filter( 'plugin_action_links_' . ACF_BOOTSTRAP_PAGE_BUILDER_PLUGIN_BASENAME, array( $this, 'action_links' ) );		
 	}
@@ -176,17 +180,80 @@ class Acf_Bootstrap_Page_Builder {
 	}
 	
 	
-		public function enqueues($hook) {
-			// Bail if user is logged out
-			if( !in_array( $hook, array( 'post.php', 'post-new.php' ) ) ) {
-				return;		
-			}			
-			wp_register_style( 'bootstrap_acf_page_builder_css', plugin_dir_url( __FILE__ ) . 'assets/css/pagebuilder.css', false, '1.0.0' );
-			wp_enqueue_style( 'bootstrap_acf_page_builder_css' );
-			wp_enqueue_script('bootstrap_acf_page_builderr_js', plugin_dir_url( __FILE__ ) . 'assets/js/pagebuilder.min.js');			
-		}
+	public function enqueues($hook) {
+		// Bail if user is logged out
+		if( !in_array( $hook, array( 'post.php', 'post-new.php' ) ) ) {
+			return;		
+		}			
+		wp_register_style( 'bootstrap_acf_page_builder_css', plugin_dir_url( __FILE__ ) . 'assets/css/pagebuilder.css', array(),  '1.1.0' );
+		wp_enqueue_style( 'bootstrap_acf_page_builder_css' );
+		wp_enqueue_script('bootstrap_acf_page_builder_js', plugin_dir_url( __FILE__ ) . 'assets/js/pagebuilder.min.js',array( 'jquery' ), 1.0);	
+		
+		add_action( 'admin_notices', array( $this, 'add_class_popup' ) );
+	}
 	
-}
+	
+	public function acfbs_settings_page($hook) {
+		add_options_page( 'ACF Bootstrap Options', 'ACF Bootstrap Page Builder', 'manage_options', 'acfbs-settings',  array( $this, 'acfbs_plugin_options' ) );
+	}
+	
+	public function acfbs_plugin_options() {
+		
+		if ( !current_user_can( 'manage_options' ) )  {
+			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+		}
+		echo '
+		<div class="wrap">
+			<h1>ACF Bootstrap Page Builder Settings</h1>
+			<p>To add new custom fields, enter the fields below</p>
+		</div>
+		';
+		
+	}
+	
+	
+	public function add_class_popup($hook) {
+		$class1 = '<div class="white-popup mfp-hide" id="page-builder-popup" data-id="" data-scrollpos=""><form name="page-builder-popup-form" id="page-builder-popup-form"><div class="container page-builder-form-container"><div class="row"><div class="col-12 col-sm-6 col-md-3 mb-4 hide-on-sections"><h3>Column width</h3><p class="mb-0 mt-3">Width</p><select class="custom-select" name="size-xs"><option selected value="col-12">12/12</option><option value="col-11">11/12</option><option value="col-10">10/12</option><option value="col-9">9/12</option><option value="col-8">8/12</option><option value="col-7">7/12</option><option value="col-6">6/12</option><option value="col-5">5/12</option><option value="col-4">4/12</option><option value="col-3">3/12</option><option value="col-2">2/12</option><option value="col-1">1/12</option></select><p class="mb-0 mt-3">Sm Width <span class="small">(576px)</span></p><select class="custom-select" name="size-sm"><option value="">-- none --</option><option value="col-sm-12">12/12</option><option value="col-sm-11">11/12</option><option value="col-sm-10">10/12</option><option value="col-sm-9">9/12</option><option value="col-sm-8">8/12</option><option value="col-sm-7">7/12</option><option value="col-sm-6">6/12</option><option value="col-sm-5">5/12</option><option value="col-sm-4">4/12</option><option value="col-sm-3">3/12</option><option value="col-sm-2">2/12</option><option value="col-sm-1">1/12</option></select><p class="mb-0 mt-3">Md Width <span class="small">(768px)</span></p><select class="custom-select" name="size-md"><option value="">-- none --</option><option value="col-md-12">12/12</option><option value="col-md-11">11/12</option><option value="col-md-10">10/12</option><option value="col-md-9">9/12</option><option value="col-md-8">8/12</option><option value="col-md-7">7/12</option><option value="col-md-6">6/12</option><option value="col-md-5">5/12</option><option value="col-md-4">4/12</option><option value="col-md-3">3/12</option><option value="col-md-2">2/12</option><option value="col-md-1">1/12</option></select><p class="mb-0 mt-3">Lg Width <span class="small">(992px)</span></p><select class="custom-select" name="size-lg"><option value="">-- none --</option><option value="col-lg-12">12/12</option><option value="col-lg-11">11/12</option><option value="col-lg-10">10/12</option><option value="col-lg-9">9/12</option><option value="col-lg-8">8/12</option><option value="col-lg-7">7/12</option><option value="col-lg-6">6/12</option><option value="col-lg-5">5/12</option><option value="col-lg-4">4/12</option><option value="col-lg-3">3/12</option><option value="col-lg-2">2/12</option><option value="col-lg-1">1/12</option></select></div><div class="col-12 col-sm-6 col-md-3 mb-4"><h3>Column Offsets</h3><p class="mb-0 mt-3">Offset</p><select class="custom-select" name="offset-xs"><option value="">-- none --</option><option value="offset-1">1</option><option value="offset-2">2</option><option value="offset-3">3</option><option value="offset-4">4</option><option value="offset-5">5</option><option value="offset-6">6</option><option value="offset-7">7</option><option value="offset-8">8</option><option value="offset-9">9</option><option value="offset-10">10</option><option value="offset-11">11</option></select><p class="mb-0 mt-3">Sm Offset <span class="small">(576px)</span></p><select class="custom-select" name="offset-sm"><option value="">-- none --</option><option value="offset-sm-1">1</option><option value="offset-sm-2">2</option><option value="offset-sm-3">3</option><option value="offset-sm-4">4</option><option value="offset-sm-5">5</option><option value="offset-sm-6">6</option><option value="offset-sm-7">7</option><option value="offset-sm-8">8</option><option value="offset-sm-9">9</option><option value="offset-sm-10">10</option><option value="offset-sm-11">11</option></select><p class="mb-0 mt-3">Md Offset <span class="small">(768px)</span></p><select class="custom-select" name="md"><option value="">-- none --</option><option value="offset-md-1">1</option><option value="offset-md-2">2</option><option value="offset-md-3">3</option><option value="offset-md-4">4</option><option value="offset-md-5">5</option><option value="offset-md-6">6</option><option value="offset-md-7">7</option><option value="offset-md-8">8</option><option value="offset-md-9">9</option><option value="offset-md-10">10</option><option value="offset-md-11">11</option></select><p class="mb-0 mt-3">Lg Offset <span class="small">(992px)</span></p><select class="custom-select" name="offset-lg"><option value="">-- none --</option><option value="offset-lg-1">1</option><option value="offset-lg-2">2</option><option value="offset-lg-3">3</option><option value="offset-lg-4">4</option><option value="offset-lg-5">5</option><option value="offset-lg-6">6</option><option value="offset-lg-7">7</option><option value="offset-lg-8">8</option><option value="offset-lg-9">9</option><option value="offset-lg-10">10</option><option value="offset-lg-11">11</option></select></div><div class="col-12 col-sm-6 col-md-6 mb-4"><h3>Formatting</h3><ul class="list-unstyled mb-0 mt-3">';
+		
+		
+		$class3 = '</ul></div><div class="col-12 col-sm-6 col-md-3 mb-4"><h3>Top Margin</h3><p class="mb-0 mt-3">Top Marg</p><select class="custom-select" name="mt-xs"><option value="">-- none --</option><option value="mt-1">1</option><option value="mt-2">2</option><option value="mt-3">3</option><option value="mt-4">4</option><option value="mt-5">5</option></select><p class="mb-0 mt-3">Sm Top Marg <span class="small">(576px)</span></p><select class="custom-select" name="mt-sm"><option value="">-- none --</option><option value="mt-sm-1">1</option><option value="mt-sm-2">2</option><option value="mt-sm-3">3</option><option value="mt-sm-4">4</option><option value="mt-sm-5">5</option></select><p class="mb-0 mt-3">Md Top Marg <span class="small">(768px)</span></p><select class="custom-select" name="mt-md"><option value="">-- none --</option><option value="mt-md-1">1</option><option value="mt-md-2">2</option><option value="mt-md-3">3</option><option value="mt-md-4">4</option><option value="mt-md-5">5</option></select><p class="mb-0 mt-3">Lg Top Marg <span class="small">(992px)</span></p><select class="custom-select" name="mt-lg"><option value="">-- none --</option><option value="mt-lg-1">1</option><option value="mt-lg-2">2</option><option value="mt-lg-3">3</option><option value="mt-lg-4">4</option><option value="mt-lg-5">5</option></select></div><div class="col-12 col-sm-6 col-md-3 mb-4"><h3>Bottom Margin</h3><p class="mb-0 mt-3">Bot Marg</p><select class="custom-select" name="mb-xs"><option value="">-- none --</option><option value="mb-1">1</option><option value="mb-2">2</option><option value="mb-3">3</option><option value="mb-4">4</option><option value="mb-5">5</option></select><p class="mb-0 mt-3">Sm Bot Marg <span class="small">(576px)</span></p><select class="custom-select" name="mb-sm"><option value="">-- none --</option><option value="mb-sm-1">1</option><option value="mb-sm-2">2</option><option value="mb-sm-3">3</option><option value="mb-sm-4">4</option><option value="mb-sm-5">5</option></select><p class="mb-0 mt-3">Md Bot Marg <span class="small">(768px)</span></p><select class="custom-select" name="mb-md"><option value="">-- none --</option><option value="mb-md-1">1</option><option value="mb-md-2">2</option><option value="mb-md-3">3</option><option value="mb-md-4">4</option><option value="mb-md-5">5</option></select><p class="mb-0 mt-3">Lg Bot Marg <span class="small">(992px)</span></p><select class="custom-select" name="mb-lg"><option value="">-- none --</option><option value="mb-lg-1">1</option><option value="mb-lg-2">2</option><option value="mb-lg-3">3</option><option value="mb-lg-4">4</option><option value="mb-lg-5">5</option></select></div><div class="col-12 col-sm-6 col-md-3 mb-4"><h3>Top Padding</h3><p class="mb-0 mt-3">Top Pad</p><select class="custom-select" name="pt-xs"><option value="">-- none --</option><option value="pt-1">1</option><option value="pt-2">2</option><option value="pt-3">3</option><option value="pt-4">4</option><option value="pt-5">5</option></select><p class="mb-0 mt-3">Sm Top Pad <span class="small">(576px)</span></p><select class="custom-select" name="pt-sm"><option value="">-- none --</option><option value="pt-sm-1">1</option><option value="pt-sm-2">2</option><option value="pt-sm-3">3</option><option value="pt-sm-4">4</option><option value="pt-sm-5">5</option></select><p class="mb-0 mt-3">Md Top Pad <span class="small">(768px)</span></p><select class="custom-select" name="pt-md"><option value="">-- none --</option><option value="pt-md-1">1</option><option value="pt-md-2">2</option><option value="pt-md-3">3</option><option value="pt-md-4">4</option><option value="pt-md-5">5</option></select><p class="mb-0 mt-3">Lg Top Pad <span class="small">(992px)</span></p><select class="custom-select" name="pt-lg"><option value="">-- none --</option><option value="pt-lg-1">1</option><option value="pt-lg-2">2</option><option value="pt-lg-3">3</option><option value="pt-lg-4">4</option><option value="pt-lg-5">5</option></select></div><div class="col-12 col-sm-6 col-md-3 mb-4"><h3>Bottom Padding</h3><p class="mb-0 mt-3">Mobile BP</p><select class="custom-select" name="pb-xs"><option value="">-- none --</option><option value="pb-1">1</option><option value="pb-2">2</option><option value="pb-3">3</option><option value="pb-4">4</option><option value="pb-5">5</option></select><p class="mb-0 mt-3">Small BP <span class="small">(576px)</span></p><select class="custom-select" name="pb-sm"><option value="">-- none --</option><option value="pb-sm-1">1</option><option value="pb-sm-2">2</option><option value="pb-sm-3">3</option><option value="pb-sm-4">4</option><option value="pb-sm-5">5</option></select><p class="mb-0 mt-3">Medium BP <span class="small">(768px)</span></p><select class="custom-select" name="pb-md"><option value="">-- none --</option><option value="pb-md-1">1</option><option value="pb-md-2">2</option><option value="pb-md-3">3</option><option value="pb-md-4">4</option><option value="pb-md-5">5</option></select><p class="mb-0 mt-3">Large BP <span class="small">(992px)</span></p><select class="custom-select" name="pb-lg"><option value="">-- none --</option><option value="pb-lg-1">1</option><option value="pb-lg-2">2</option><option value="pb-lg-3">3</option><option value="pb-lg-4">4</option><option value="pb-lg-5">5</option></select></div></div><div class="row mt-3"><div class="col-12"><a href="#" class="btn btn-primary" id="submit-styles-btn">Apply Styles</a><a href="#" class="btn btn-outline-primary ml-3" id="reset-styles-btn">Reset</a></div></div></div></form></div>';
+		
+		$class2 = '';		
+		
+		if ( false === ( $content = get_transient( 'acfbs_classes'))) {			
+		
+			if( have_rows('acfbs_classes','option') ):
+				while ( have_rows('acfbs_classes','option') ) : the_row();
+					$class = get_sub_field('class');
+					$label = get_sub_field('label');
+					$type = get_sub_field('type');		
+					$class2 .= '<li class="show-'.$type.'"><input type="checkbox" value="'.$class.'"><label for="'.$class.'">'.$label.'</label></li>';
+				endwhile;
+			else: 
+				$class2 = '<li>Enter custom classes on the <a href="'.get_home_url().'/wp-admin/admin.php?page=acf-options-page-builder" target="_blank">'.get_bloginfo('name').' Settings</a> page</li>';
+			endif;
+
+			$content = $class1.$class2.$class3;
+			
+			if ( ! empty( $content ) ) {
+				set_transient( 'acfbs_classes', $content, 1 * YEAR_IN_SECONDS );
+			}		
+		} 
+		if ( ! empty( $content ) ) {
+			echo $content;
+		}
+	}	
+	
+	
+	public function clear_acfbs_transient() {
+		$screen = get_current_screen();
+		if (strpos($screen->id, "acf-options-page-builder") == true) {
+			delete_transient('acfbs_classes');
+		}
+	}
+	
+	
+	}
 
 endif;
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-// Initialize modules
+'use strict';
+
 // Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
 const { src, dest, watch, series, parallel } = require('gulp');
 // Importing all the Gulp-related packages we want to use
@@ -9,47 +10,52 @@ const uglify = require('gulp-uglify');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const notify = require('gulp-notify');
+const rename = require( 'gulp-rename' );
 var replace = require('gulp-replace');
 
 
 // File paths
 const files = { 
-    scssPath: './assets/css/sass/**/*.scss',
-    jsPath: './assets/js/custom/*.js',
-	styleDest: './assets/css/',
-	jsDest: './assets/js/'
+    scssPath: 'assets/scss/**/*.scss',
+    jsPath: 'assets/js/custom/*.js',
+	styleDest: 'assets/css/',
+	jsDest: 'assets/js/',
+	jsName: 'pagebuilder'
 }
 
 // Sass task: compiles the style.scss file into style.css
 function scssTask(){    
-    return src(files.scssPath)
+    return src('assets/scss/pagebuilder.scss')
         .pipe(sourcemaps.init()) // initialize sourcemaps first
         .pipe(sass()) // compile SCSS to CSS
+		.on("error", sass.logError)
         .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
         .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
-        .pipe(dest(files.styleDest)
-    ); // put final CSS in dist folder
+        .pipe(dest('assets/css'))
+		.pipe(notify({ message: 'TASK: "CSS" Completed!'}));
 }
 
+	
 // JS task: concatenates and uglifies JS files to script.js
 function jsTask(){
     return src([
-        files.jsPath
+       files.jsPath		
         //,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
         ])
-        .pipe(concat('pagebuilder.min.js'))
+        .pipe(concat(files.jsName + '.js'))
+		.pipe(
+			rename({
+				basename: files.jsName,
+				suffix: '.min'
+			})
+		)
         .pipe(uglify())
-        .pipe(dest(files.jsDest)
-    );
+        .pipe(dest(files.jsDest));	
+		//.pipe(notify({ message: 'TASK: "JS" Completed!'}))
 }
 
-// Cachebust
-var cbString = new Date().getTime();
-function cacheBustTask(){
-    return src(['index.html'])
-        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
-        .pipe(dest('.'));
-}
+
 
 // Watch task: watch SCSS and JS files for changes
 // If any change, run scss and js tasks simultaneously
@@ -63,6 +69,5 @@ function watchTask(){
 // then runs cacheBust, then watch task
 exports.default = series(
     parallel(scssTask, jsTask), 
-    //cacheBustTask,
     watchTask
 );
